e4597c215621fc6bfaa72e92103363de8016a6b4
Amil Masic <amil.masic@gmail.com>
2018-09-12 00:54:11 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,6 +4,6 @@ def line(array)
       puts "The line is currently empty."
   else
     array.size <=1
-    puts "The line is currently: "
+    puts "The line is currently: #{array[]}"
     end
   end
