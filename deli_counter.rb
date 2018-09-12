9b3ae70fa4b33da98f9105bd17163ceaa859abf6
Amil Masic <amil.masic@gmail.com>
2018-09-12 01:07:50 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,16 +1,20 @@
 # Write your code here.
 require "pry"
 def line(array)
+  # checks if the line is empty
   if array.size == 0
       puts "The line is currently empty."
     else
     # defined an empty array to store itterated values in
     arr_line = []
     array.each_with_index do |name, index|
+      # making index start from 1
       index +=1
+      # pushing values into new array
       arr_line << "#{index}. #{name}"
     end
     # binding.pry
+    # displaying the message while also converting array to string
     puts "The line is currently: " + arr_line.join(" ")
   end
   end
