f06747b3bd4239fa5922330e2104b8df5a97ec8b
Amil Masic <amil.masic@gmail.com>
2018-09-12 00:49:24 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
 # Write your code here.
+def line(array)
+  if array.size == 0
+      puts "The line is currently empty."
+  else
+    array.each_with_index do |name, index|
+      # index.to_i
+      # index += 1
+      new_string = array.join("#{index}. #{name}")
+      puts new_string
+    end
+  end
+end
