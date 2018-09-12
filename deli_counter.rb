abfa1e709e36391ec6de1bb360f66890f3e24f2a
Amil Masic <amil.masic@gmail.com>
2018-09-12 00:53:04 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,11 +3,8 @@ def line(array)
   if array.size == 0
       puts "The line is currently empty."
   else
-    array.each_with_index do |name, index|
-      # index.to_i
-      # index += 1
-      new_string = array.join("#{index}. #{name}")
-      puts new_string
+    array.size <=1 
+    puts "The line is currently: "
     end
   end
 end
